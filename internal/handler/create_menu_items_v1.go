8e8b3c1edef3ee2381601f015563ae06a142cdf1
Hikaru Saito <beretta0602@gmail.com>
2026-05-08 15:21:19 +0900

    スタブ実装

---
+++ internal/handler/create_menu_items_v1.go
@@ -0,0 +1,12 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+
+	api "github.com/fun-dotto/funch-api/generated"
+)
+
+func (h *Handler) MenuItemsV1Create(ctx context.Context, request api.MenuItemsV1CreateRequestObject) (api.MenuItemsV1CreateResponseObject, error) {
+	return nil, fmt.Errorf("not implemented")
+}
